41b193d8625c1ab1bc484d7ac76eeae45ac6f0cc
ismoyo23 <63132957+ismoyo23@users.noreply.github.com>
2020-04-18 19:14:54 +0700

    initial commit

--- soal5.js
+++ soal5.js
@@ -1,7 +1,7 @@
 function matrix(m, n) {
     var result = []
     for(var i = 0; i < n; i++) {
-        result.push(new Array(m).fill(Math.floor(Math.random() * 201) - 100))
+        result.push(new Array(m).fill(Math.floor(Math.random() * 99)))
     }
     return result
 }
